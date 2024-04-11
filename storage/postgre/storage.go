cd0f2b4f115d6591682e1df65ea170320263afcd
Lavander40 <a.kirill.03@mail.ru>
2024-04-11 23:29:48 +0300

    not last

--- storage/postgre/storage.go
+++ storage/postgre/storage.go
@@ -20,7 +20,7 @@ func New(config *config.Config) *Postgre {
 }
 
 func (p *Postgre) Open() error {
-	conn, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", p.conf.POSTGRES_USER, p.conf.POSTGRES_PASSWORD, "postgre", "5432", p.conf.POSTGRES_DB))
+	conn, err := sql.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", p.conf.POSTGRES_USER, p.conf.POSTGRES_PASSWORD, "postgres", "5432", p.conf.POSTGRES_DB))
 	if err != nil {
 		return err
 	}
